79b82a2ddab6ab79d6498da9bf84a916c27f50b8
sswain2 <sswain2@RDINAPDEVET20>
2026-02-06 15:17:41 +0530

    Add project files.

---
+++ EmployeeApp/Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿using EmployeeMiniApp.Data;
+using EmployeeMiniApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeMiniApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var employees = _context.Employees.ToList();
+            return View(employees);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Employees.Add(emp);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(emp);
+        }
+    }
+}
---
+++ EmployeeApp/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeMiniApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Department { get; set; }
+
+        public decimal Salary { get; set; }
+    }
+}
